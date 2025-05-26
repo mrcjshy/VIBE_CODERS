2537acd2fbb326e93ef8473e91d2eb3f27447689
DarrylCandilada-2I <dccandilada@paterostechnologicalcollege.edu.ph>
2025-05-26 20:46:20 +0800

    Add Database Connection

--- inventory-system-project/backend/test-connection.js
+++ inventory-system-project/backend/test-connection.js
@@ -1,6 +1,8 @@
+// DARRYL YAM C. CANDILADA - BSIT 2-I
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('inventory_db', 'postgres', '09057644429', {
+const sequelize = new Sequelize('inventory_db', 'postgres', '1010', {
   host: '127.0.0.1',
   dialect: 'postgres'
 });
